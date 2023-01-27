513eb045e356e0b967430c292c91db36b198525a
Russell Gordon <rgordon@lcs.on.ca>
2023-01-27 13:19:18 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList
+//
+//  Created by Russell Gordon on 2023-01-27.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationsListView()
+    }
+}
